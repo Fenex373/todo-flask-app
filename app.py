1ef910d9c7bd3ecbacdf16e05376be02a6a64181
Piotr <piotrek07112008@gmail.com>
2025-05-15 00:30:00 +0200

    Dodanie os

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+import os
+
+
+
 
 app = Flask(__name__)
 
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///local.db")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret'
